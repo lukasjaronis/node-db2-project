ee859efafdd9130ac18ffaff0155968fde8ff12a
Lukas Jaronis <jaronisluke13@gmail.com>
2020-01-22 22:23:49 -0500

    edited kexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,22 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: "./data/car-dealer.db3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
     }
-  },
-
+  }
 };
